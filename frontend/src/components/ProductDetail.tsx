29b110f6f01d4e8ede7494fdaa25cbe44c3eea71
Niklas <nkgr02@gmail.com>
2024-06-07 11:43:21 +0200

    small fix

--- frontend/src/components/ProductDetail.tsx
+++ frontend/src/components/ProductDetail.tsx
@@ -2,7 +2,6 @@ import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import NavBar from "./NavBar";
 import Footer from "./Footer";
-import User from "./User";
 
 interface Product {
   productID: number;
